52204c6641feb1802228a8675cb44f9f6cec66ab
muhammadumerjaved44 <muhammadumerjaved44@gmail.com>
2022-08-14 03:43:56 +0500

    change port

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ def task_delete(id):
     return {"status": status, "detail": "Task deleted successfully", "data": response}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
